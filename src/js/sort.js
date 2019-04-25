4f8c6166266a7d9b08ec63b5d3753e1868271bcf
dingli <dingli@360.net>
2019-04-25 14:56:18 +0800

    qsort‘

--- src/js/sort.js
+++ src/js/sort.js
@@ -92,3 +92,25 @@ function partition(arr, left, right) {
     arr[pivot] = arr.splice(index - 1, 1, arr[pivot])[0]; // 交换顺序时，以最后一位替换分隔项
     return index - 1;
 }
+function _quickSort(arr) {
+    //如果数组<=1,则直接返回
+    if (arr.length <= 1) { return arr; }
+    var pivotIndex = Math.floor(arr.length / 2);
+    //找基准，并把基准从原数组删除
+    var pivot = arr.splice(pivotIndex, 1)[0];
+    //定义左右数组
+    var left = [];
+    var right = [];
+
+    //比基准小的放在left，比基准大的放在right
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] <= pivot) {
+            left.push(arr[i]);
+        }
+        else {
+            right.push(arr[i]);
+        }
+    }
+    //递归
+    return _quickSort(left).concat([pivot], _quickSort(right));
+}
